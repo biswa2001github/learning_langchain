06b4ded85f69930fa03583976a9b49cead76126a
Biswadeep Debnath <biswadeepdebnath.dmr1@gmail.com>
2026-07-16 11:16:23 +0530

    tested gemini free models

---
+++ 1_llms/1_llm_demo.py
@@ -0,0 +1,12 @@
+from langchain_google_genai import GoogleGenerativeAI
+from dotenv import load_dotenv
+
+# This will load environment variables
+load_dotenv()
+
+# Initilaise LLM
+llm = GoogleGenerativeAI(model="gemini-3.5-flash")
+
+result = llm.invoke("What is United Nations?")
+
+print(result)
